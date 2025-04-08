061f7cc8d0e151be52962332e5b5988a55f401e4
SK0014 <63155286+SK0014@users.noreply.github.com>
2025-04-08 18:47:08 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,49 @@
-module govulnapp
+package main
 
-go 1.18
-
-require (
-    github.com/gin-gonic/gin v1.6.3           // CVE-2020-28483
-    github.com/dgrijalva/jwt-go v3.2.0+incompatible // CVE-2020-26160
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/dgrijalva/jwt-go"
+    "html/template"
+    "net/http"
+    "time"
 )
+
+var secretKey = []byte("secret")
+
+func main() {
+    router := gin.Default()
+    router.SetHTMLTemplate(template.Must(template.ParseFiles("templates/index.html")))
+
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", gin.H{"token": ""})
+    })
+
+    router.POST("/token", func(c *gin.Context) {
+        user := c.PostForm("user")
+
+        // 🔥 Creates an unsigned token (vulnerable to CVE-2020-26160)
+        token := jwt.NewWithClaims(jwt.SigningMethodNone, jwt.MapClaims{
+            "user": user,
+            "exp":  time.Now().Add(time.Hour * 1).Unix(),
+        })
+        tokenString, _ := token.SignedString(jwt.UnsafeAllowNoneSignatureType)
+
+        c.HTML(http.StatusOK, "index.html", gin.H{"token": tokenString})
+    })
+
+    // 🔥 Insecure JWT parsing
+    router.GET("/validate", func(c *gin.Context) {
+        tokenString := c.Query("token")
+        token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+            return secretKey, nil
+        })
+
+        if token != nil && token.Valid {
+            c.String(http.StatusOK, "Token is valid!")
+        } else {
+            c.String(http.StatusBadRequest, "Invalid token!")
+        }
+    })
+
+    router.Run(":8080")
+}
